a7940c340ee75ce39edbc7505081f9035f4b8b5a
whitelizard <esbblo@gmail.com>
2018-07-29 12:40:00 +0200

    WIP

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
 import pt from 'prop-types';
 
 export const transformable = pt.array;
-export const transformableOr = typeSpec =>
-  pt.oneOfType([typeSpec, transformable]);
+export const transformableOr = typeSpec => pt.oneOfType([typeSpec, transformable]);
 
 // ----- GENERIC DATA MODEL ----------------------------------------
 
-export const assetTypeMinimum = {
-  id: pt.string.isRequired,
-};
-export const assetTypeMinimumShape = pt.shape(assetTypeMinimum);
-
-export const rpcMinimum = {
-  id: pt.string.isRequired,
-};
-export const rpcMinimumShape = pt.shape(rpcMinimum);
-
 export const assetMinimum = {
+  rid: pt.string.isRequired,
   id: pt.string.isRequired,
   name: pt.string.isRequired,
 };
 export const assetMinimumShape = pt.shape(assetMinimum);
 
-export const thingMinimum = {};
+export const assetTypeMinimum = {
+  rid: pt.string.isRequired,
+  id: pt.string.isRequired,
+};
+export const assetTypeMinimumShape = pt.shape(assetTypeMinimum);
+
+export const thingMinimum = {
+  rid: pt.string.isRequired,
+};
 export const thingMinimumShape = pt.shape(thingMinimum);
 
-export const configMinimum = {};
-export const configMinimumShape = pt.shape(configMinimum);
-
-export const thingTypeMinimum = {};
+export const thingTypeMinimum = {
+  rid: pt.string.isRequired,
+};
 export const thingTypeMinimumShape = pt.shape(thingTypeMinimum);
 
-export const channelTypeMinimum = {};
-export const channelTypeMinimumShape = pt.shape(channelTypeMinimum);
+export const configMinimum = {
+  rid: pt.string.isRequired,
+};
+export const configMinimumShape = pt.shape(configMinimum);
 
 export const roleMinimum = {
   // TODO: specify!
+  rid: pt.string.isRequired,
   name: pt.string,
   sub: pt.object,
   pub: pt.object,
@@ -45,31 +45,46 @@ export const roleMinimumShape = pt.shape(roleMinimum);
 
 export const ruleMinimum = {
   // TODO: specify!
+  rid: pt.string.isRequired,
   name: pt.string,
   active: pt.bool,
-  ttl: pt.number,
   triggers: pt.object,
   externalData: pt.object,
+  process: pt.object,
   condition: pt.object,
-  resetCondition: pt.object,
   actions: pt.object,
+  resetCondition: pt.object,
   resetActions: pt.object,
   cooldown: pt.number,
+  ttl: pt.number,
   actuator: pt.string,
 };
 export const ruleMinimumShape = pt.shape(ruleMinimum);
 
 export const userMinimum = {
+  rid: pt.string.isRequired,
   id: pt.string.isRequired,
   name: pt.string.isRequired,
 };
 export const userMinimumShape = pt.shape(userMinimum);
 
 export const channelMinimum = {
+  rid: pt.string.isRequired,
   id: pt.string.isRequired,
   historyConf: pt.object, // TODO: specify
 };
 export const channelMinimumShape = pt.shape(channelMinimum);
+
+export const channelTypeMinimum = {
+  rid: pt.string.isRequired,
+};
+export const channelTypeMinimumShape = pt.shape(channelTypeMinimum);
+
+export const rpcMinimum = {
+  rid: pt.string.isRequired,
+  id: pt.string.isRequired,
+};
+export const rpcMinimumShape = pt.shape(rpcMinimum);
 
 // ----- SPECIALICED DATA MODEL ----------------------------------------
 
