c23eb772ccf5ac056fce0d44d79759de84f5196d
Jesper Ahlberg <jesper.ahlberg@cybercom.com>
2018-08-29 20:19:10 +0200

    Publish of first draft

---
+++ src/index.js
@@ -0,0 +1,67 @@
+import { Asset } from './Asset';
+import { AssetType } from './AssetType';
+import { Channel } from './Channel';
+import { ChannelType } from './ChannelType';
+import { Configuration } from './Configuration';
+import { HasChannel } from './HasChannel';
+import { HasChild } from './HasChild';
+import { HasConfiguration } from './HasConfiguration';
+import { HasProcedure } from './HasProcedure';
+import { HasRole } from './HasRole';
+import { HasType } from './HasType';
+import { Identity } from './Identity';
+import { Procedure } from './Procedure';
+import { Role } from './Role';
+import { Rule } from './Rule';
+import { Thing } from './Thing';
+import { ThingType } from './ThingType';
+import { User } from './User';
+
+const Vertices = {
+  Asset,
+  AssetType,
+  Channel,
+  ChannelType,
+  Configuration,
+  Identity,
+  Procedure,
+  Role,
+  Rule,
+  Thing,
+  ThingType,
+  User,
+};
+
+const Edges = {
+  HasChannel,
+  HasChild,
+  HasConfiguration,
+  HasProcedure,
+  HasRole,
+  HasType,
+};
+
+const datamodel = {
+  Asset,
+  AssetType,
+  Channel,
+  ChannelType,
+  Configuration,
+  HasChannel,
+  HasChild,
+  HasConfiguration,
+  HasProcedure,
+  HasRole,
+  HasType,
+  Identity,
+  Procedure,
+  Role,
+  Rule,
+  Thing,
+  ThingType,
+  User,
+  Vertices,
+  Edges,
+};
+
+export default datamodel;
