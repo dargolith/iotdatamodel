c7ce2f5e2a4bc3bb98e32000de0b7d046f577674
Jesper Ahlberg <jesper.ahlberg@cybercom.com>
2018-11-21 17:12:39 +0100

    Starting edge schema creation (WIP)

--- src/configuration/Asset.js
+++ src/configuration/Asset.js
@@ -1,4 +1,6 @@
 import joi from 'joi';
+import HasType from './HasType';
+import AssetType from './AssetType';
 
 export const Asset = joi
   .object()
@@ -10,8 +12,23 @@ export const Asset = joi
       .required()
       .meta({ unique: true }),
     name: joi.string().required(),
-    // type: joi.string(),
-    // channels: joi.object().pattern(joi.any(), joi.string()),
-    // children: joi.array().items(joi.string()),
-    // parents: joi.array().items(joi.string()),
   });
+
+export const AssetDoc = Asset.keys({
+  type: joi.string(),
+  channels: joi.object().pattern(joi.any(), joi.string()),
+  children: joi.array().items(joi.string()),
+  parents: joi.array().items(joi.string()),
+});
+
+export const AssetEdges = joi.object().keys({
+  // TODO: WIP
+  out: joi.object().keys({
+    type: joi.object().meta({
+      class: HasType,
+      multiple: false,
+      target: AssetType,
+    }),
+  }),
+  in: joi.object().keys(),
+});
